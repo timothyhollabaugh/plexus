276db9dcb554429569a95cb56b6026623b77dce3
Sean Olson <olson.sean.k@gmail.com>
2017-12-18 00:09:22 -0800

    Refactor PLY traits.

--- src/geometry/ply.rs
+++ src/geometry/ply.rs
@@ -2,6 +2,7 @@
 
 use decorum::{Ordered, Primitive};
 use num::Float;
+use ply_rs::ply::{self, DefaultElement, PropertyAccess};
 use std::io::{Read, Write};
 
 use geometry::{Attribute, Geometry, Triplet};
@@ -9,16 +10,54 @@ use graph::Mesh;
 
 pub use ply_rs::ply::{Encoding, PropertyType, ScalarType};
 
-pub trait Property {
+pub type PropertyMap = DefaultElement;
+
+pub trait Property: Sized {
     const PLY_TYPE: PropertyType;
+
+    fn read<A>(name: &str, properties: &A) -> Result<Self, ()>
+    where
+        A: PropertyAccess;
+
+    fn write<A>(&self, name: &str, properties: &mut A)
+    where
+        A: PropertyAccess;
 }
 
 impl Property for f32 {
     const PLY_TYPE: PropertyType = PropertyType::Scalar(ScalarType::Float);
+
+    fn read<A>(name: &str, properties: &A) -> Result<Self, ()>
+    where
+        A: PropertyAccess,
+    {
+        properties.get_float(&name.to_owned()).ok_or(())
+    }
+
+    fn write<A>(&self, name: &str, properties: &mut A)
+    where
+        A: PropertyAccess,
+    {
+        properties.set_property(name.to_owned(), ply::Property::Float(*self));
+    }
 }
 
 impl Property for f64 {
     const PLY_TYPE: PropertyType = PropertyType::Scalar(ScalarType::Double);
+
+    fn read<A>(name: &str, properties: &A) -> Result<Self, ()>
+    where
+        A: PropertyAccess,
+    {
+        properties.get_double(&name.to_owned()).ok_or(())
+    }
+
+    fn write<A>(&self, name: &str, properties: &mut A)
+    where
+        A: PropertyAccess,
+    {
+        properties.set_property(name.to_owned(), ply::Property::Double(*self));
+    }
 }
 
 impl<T> Property for Ordered<T>
@@ -26,35 +65,51 @@ where
     T: Float + Primitive + Property,
 {
     const PLY_TYPE: PropertyType = T::PLY_TYPE;
+
+    fn read<A>(name: &str, properties: &A) -> Result<Self, ()>
+    where
+        A: PropertyAccess,
+    {
+        Ok(Ordered::from_raw_float(
+            <T as Property>::read(name, properties)?,
+        ))
+    }
+
+    fn write<A>(&self, name: &str, properties: &mut A)
+    where
+        A: PropertyAccess,
+    {
+        self.clone().into_raw_float().write(name, properties);
+    }
 }
 
 pub trait Element: Attribute {
-    fn properties<'a>() -> &'a [(&'static str, PropertyType)];
+    fn property_definitions<'a>() -> &'a [(&'static str, PropertyType)];
 
-    fn read<T>() -> Result<Self, ()>
+    fn read<A>(properties: &A) -> Result<Self, ()>
     where
-        T: Read;
+        A: PropertyAccess;
 
-    fn write<T>(&self) -> Result<(), ()>
+    fn write<A>(&self, properties: &mut A) -> Result<(), ()>
     where
-        T: Write;
+        A: PropertyAccess;
 }
 
 impl Element for () {
-    fn properties<'a>() -> &'a [(&'static str, PropertyType)] {
+    fn property_definitions<'a>() -> &'a [(&'static str, PropertyType)] {
         &[]
     }
 
-    fn read<T>() -> Result<Self, ()>
+    fn read<A>(_: &A) -> Result<Self, ()>
     where
-        T: Read,
+        A: PropertyAccess,
     {
         Ok(())
     }
 
-    fn write<T>(&self) -> Result<(), ()>
+    fn write<A>(&self, _: &mut A) -> Result<(), ()>
     where
-        T: Write,
+        A: PropertyAccess,
     {
         Ok(())
     }
@@ -64,26 +119,40 @@ impl<P> Element for Triplet<P>
 where
     P: Clone + Property,
 {
-    fn properties<'a>() -> &'a [(&'static str, PropertyType)] {
+    fn property_definitions<'a>() -> &'a [(&'static str, PropertyType)] {
         &[("x", P::PLY_TYPE), ("y", P::PLY_TYPE), ("z", P::PLY_TYPE)]
     }
 
-    fn read<T>() -> Result<Self, ()>
+    fn read<A>(properties: &A) -> Result<Self, ()>
     where
-        T: Read,
+        A: PropertyAccess,
     {
-        Err(())
+        let mut values = Self::property_definitions()
+            .iter()
+            .map(|&(name, _)| P::read(name, properties).unwrap());
+        Ok(Triplet(
+            values.next().unwrap(),
+            values.next().unwrap(),
+            values.next().unwrap(),
+        ))
     }
 
-    fn write<T>(&self) -> Result<(), ()>
+    fn write<A>(&self, properties: &mut A) -> Result<(), ()>
     where
-        T: Write,
+        A: PropertyAccess,
     {
-        Err(())
+        self.0.write("x", properties);
+        self.1.write("y", properties);
+        self.2.write("z", properties);
+        Ok(())
     }
 }
 
 pub trait Ply: Geometry {
+    fn read<T>(input: &T) -> Result<Mesh<Self>, ()>
+    where
+        T: Read;
+
     fn write<T>(output: &mut T, encoding: Encoding, mesh: &Mesh<Self>) -> Result<(), ()>
     where
         T: Write;
@@ -96,6 +165,13 @@ where
     G::Edge: Element,
     G::Face: Element,
 {
+    fn read<T>(input: &T) -> Result<Mesh<Self>, ()>
+    where
+        T: Read,
+    {
+        Err(())
+    }
+
     fn write<T>(output: &mut T, encoding: Encoding, mesh: &Mesh<Self>) -> Result<(), ()>
     where
         T: Write,
@@ -114,22 +190,32 @@ mod feature_geometry_nalgebra {
     where
         P: Property + Scalar,
     {
-        fn properties<'a>() -> &'a [(&'static str, PropertyType)] {
+        fn property_definitions<'a>() -> &'a [(&'static str, PropertyType)] {
             &[("x", P::PLY_TYPE), ("y", P::PLY_TYPE), ("z", P::PLY_TYPE)]
         }
 
-        fn read<T>() -> Result<Self, ()>
+        fn read<A>(properties: &A) -> Result<Self, ()>
         where
-            T: Read,
+            A: PropertyAccess,
         {
-            Err(())
+            let mut values = Self::property_definitions()
+                .iter()
+                .map(|&(name, _)| P::read(name, properties).unwrap());
+            Ok(Point3::new(
+                values.next().unwrap(),
+                values.next().unwrap(),
+                values.next().unwrap(),
+            ))
         }
 
-        fn write<T>(&self) -> Result<(), ()>
+        fn write<A>(&self, properties: &mut A) -> Result<(), ()>
         where
-            T: Write,
+            A: PropertyAccess,
         {
-            Err(())
+            self.x.write("x", properties);
+            self.y.write("y", properties);
+            self.z.write("z", properties);
+            Ok(())
         }
     }
 }
